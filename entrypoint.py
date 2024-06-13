e13d7a70ff6bde396e0ad9fd05f05708036a8c99
fallbackerik <fallbackerik@gmail.com>
2024-06-13 11:08:05 +0200

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -7,4 +7,11 @@ import xml.etree.ElementTree as etree
 SVG_NAMESPACE = "https://www.web3.org/2000/svg"
 
 for file in glob.glob(sys.argv[2]):
-  print(file)
+  filebase = os.path.basename(file)
+  print("opening", filebase, "...")
+  tree = etree.parseFile(file)
+  root = tree.getroot()
+
+  if not "svg" in root.attrib.get("xmlns", "attrib doesn't exist):
+    root.attrib["xmlns"] = SVG_NAMESPACE
+    tree = tree.ElementTree(root)
