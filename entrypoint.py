a6b024f9dacfced23e061fc9308f8a1f218776f2
fallbackerik <fallbackerik@gmail.com>
2024-06-13 11:18:40 +0200

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -9,7 +9,7 @@ SVG_NAMESPACE = "https://www.web3.org/2000/svg"
 for file in glob.glob(sys.argv[2]):
   filebase = os.path.basename(file)
   print("opening", filebase, "...")
-  tree = etree.parseFile(file)
+  tree = etree.parse(file)
   root = tree.getroot()
 
   if not "svg" in root.attrib.get("xmlns", "attrib doesn't exist"):
