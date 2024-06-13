0b292627f259d69593401c5186f6da8b17aa3a83
fallbackerik <fallbackerik@gmail.com>
2024-06-13 11:03:41 +0200

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -1,1 +1,9 @@
-raise Exception("Testing")
+import sys
+import os
+
+import xml.etree.ElementTree as etree
+
+SVG_NAMESPACE = "https://www.web3.org/2000/svg"
+
+for file in glob.glob(sys.argv[2]):
+  print(file)
