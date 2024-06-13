d70d57c18130f77f155cdeec978d0afa7c7f6856
fallbackerik <fallbackerik@gmail.com>
2024-06-13 16:19:17 +0200

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -10,24 +10,34 @@ namespaces = {
   "ns0" : SVG_NAMESPACE,
 }
 
-for file in glob.glob(sys.argv[2]):
-  filebase = os.path.basename(file)
-  print("opening", filebase, "...")
-  tree = etree.parse(file)
-  print("tree", tree)
-  root = tree.getroot()
-  print("root", root)
+#for file in glob.glob(sys.argv[2]):
+#  filebase = os.path.basename(file)
+#  print("opening", filebase, "...")
+#  tree = etree.parse(file)
+#  print("tree", tree)
+#  root = tree.getroot()
+#  print("root", root)
+#
+#  #if not "svg" in root.attrib.get("xmlns", "attrib doesn't exist"):
+#  #  print("setting xmlns, because: ", root.attrib.get("xmlns", "attrib doesn't exist"))
+#  #  root.attrib["xmlns"] = SVG_NAMESPACE
+#  #  tree = etree.ElementTree(root)
+#
+#  if "width" not in root.attrib and "height" not in root.attrib:
+#    raise ValueException("expected width and height attributes in svg root element")
+#
+#  width = int(root.attrib["width"])
+#  height = int(root.attrib["height"])
+#
+#  print("xpath", tree.xpath("svg"))
+#  print("...", tree.xpath(".//circle"))
 
-  #if not "svg" in root.attrib.get("xmlns", "attrib doesn't exist"):
-  #  print("setting xmlns, because: ", root.attrib.get("xmlns", "attrib doesn't exist"))
-  #  root.attrib["xmlns"] = SVG_NAMESPACE
-  #  tree = etree.ElementTree(root)
+root = etree.Element("root")
 
-  if "width" not in root.attrib and "height" not in root.attrib:
-    raise ValueException("expected width and height attributes in svg root element")
+root.append( etree.Element("child") )
+etree.SubElement(root, "child")
+etree.SubElement(root, "child")
 
-  width = int(root.attrib["width"])
-  height = int(root.attrib["height"])
+print("generated etree", etree.tostring(root))
+print("...", root.xpath(".//child"))
 
-  print("xpath", tree.xpath("svg"))
-  print("...", tree.xpath(".//circle"))
