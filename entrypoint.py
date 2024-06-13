0b8e499882a21a6539187edbd7d7ec62fad3f39b
fallbackerik <fallbackerik@gmail.com>
2024-06-13 11:34:03 +0200

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -5,6 +5,7 @@ import glob
 import xml.etree.ElementTree as etree
 
 SVG_NAMESPACE = "https://www.web3.org/2000/svg"
+namespaces = { "svg" : SVG_NAMESPACE }
 
 for file in glob.glob(sys.argv[2]):
   filebase = os.path.basename(file)
@@ -15,3 +16,11 @@ for file in glob.glob(sys.argv[2]):
   if not "svg" in root.attrib.get("xmlns", "attrib doesn't exist"):
     root.attrib["xmlns"] = SVG_NAMESPACE
     tree = etree.ElementTree(root)
+
+  if "width" not in root.attrib and "height" not in root.attrib:
+    raise ValueException("expected width and height attributes in svg root element")
+
+  width = int(root.attrib["width"])
+  height = int(root.attrib["height"])
+
+  print(root.findall(".//svg:circle", namespaces))
